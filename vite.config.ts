76560e5f47e6c09c685ceb0c2c410b57cdd0cb58
Andrew Bubnov <79826346992@ya.ru>
2025-11-25 08:48:50 +0000

    chore: add fetures layer

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 export default defineConfig({
   resolve: {
     alias: {
+      "@features": path.resolve(__dirname, "./src/features"),
       "@shared": path.resolve(__dirname, "./src/shared"),
     },
   },
